b3001479e2b34d83234d08c4fa53b0e39dcce7ed
Pablo Cortes <aleacevedo1304@gmail.comgit config --global user.email>
2026-07-16 17:20:33 -0600

    Mejora la navegación del menú con JavaScript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+const enlaces = document.querySelectorAll('nav a');
+
+enlaces.forEach(enlace => {
+
+    enlace.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        const destino = document.querySelector(this.getAttribute("href"));
+
+        destino.scrollIntoView({
+
+            behavior:"smooth"
+
+        });
+
+    });
+
+});
+
 const btnArriba = document.getElementById("btnArriba");
 
 window.addEventListener("scroll", () => {
